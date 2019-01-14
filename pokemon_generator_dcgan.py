49b1e8bf2fb77978514b07bd6dbaf7aad9704373
arrouasserudy <34417076+arrouasserudy@users.noreply.github.com>
2019-01-14 10:57:46 +0200

    Update pokemon_generator_dcgan.py

--- pokemon_generator_dcgan.py
+++ pokemon_generator_dcgan.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.parallel
 import torch.optim as optim
-import torch.utils.data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
